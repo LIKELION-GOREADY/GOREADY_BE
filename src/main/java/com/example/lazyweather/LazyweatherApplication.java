cc6a6dd6247364943df99085a20ef98672305093
oosedus <seayeon0729@gmail.com>
2024-10-10 15:39:52 +0900

    초기 프로젝트 세팅

---
+++ src/main/java/com/example/lazyweather/LazyweatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.lazyweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyweatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LazyweatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lazyweather/LazyweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lazyweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyweatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
